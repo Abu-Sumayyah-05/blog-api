9c319007ca420d2d610d8e039e0f4c9bf779ac81
Abu Sumayyah <ahmadsubair@gmail.com>
2026-01-22 08:22:27 +0100

    build error fixed

--- blog-app/src/router/index.ts
+++ blog-app/src/router/index.ts
@@ -3,11 +3,13 @@ import Home from "../pages/Home.vue"
 import PostDetail from "../pages/PostDetail.vue"
 import NotFound from "../pages/NotFound.vue"
 
+const routes = [
+  { path: "/", component: Home },
+  { path: "/posts/:id", component: PostDetail },
+  { path: "/:pathMatch(.*)*", component: NotFound },
+]
+
 export const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    { path: "/", component: Home },
-    { path: "/posts/:id", component: PostDetail },
-    { path: "/:pathMatch(.*)*", component: NotFound }
-  ]
+  routes,
 })
