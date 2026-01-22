068ad4eee7ba2f788450b19538dd80c72343fc60
Abu Sumayyah <ahmadsubair@gmail.com>
2026-01-22 08:42:57 +0100

    fixing deployment issues

--- blog-app/vite.config.ts
+++ blog-app/vite.config.ts
@@ -3,6 +3,9 @@ import vue from "@vitejs/plugin-vue"
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
+  build: {
+    outDir: "dist",
+  },
   plugins: [
     vue(),
     tailwindcss(),
